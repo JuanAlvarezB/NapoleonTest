1cd4b5dd0e2f0ecfd1750c806bc07d3974e3980a
JuanAlvarezB <60496780+JuanAlvarezB@users.noreply.github.com>
2020-06-13 19:25:26 -0500

    Update RunnerTags.java

--- src/test/java/runners/RunnerTags.java
+++ src/test/java/runners/RunnerTags.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = {"src/test/resources/features/"},
-        tags = "@stories",
+        tags = "@stage1",
         glue = "definitions",
         snippets = SnippetType.CAMELCASE )
 
